e544de3191682a6dbff9f5145936bf5f7d630f22
DarkSky <darksky2048@gmail.com>
2024-11-05 20:03:15 +0800

    feat: move router to backend

--- tools/cli/src/webpack/index.ts
+++ tools/cli/src/webpack/index.ts
@@ -367,6 +367,8 @@ export function createWebpackConfig(
           changeOrigin: true,
           secure: false,
         },
+        { context: '/oauth/login', target: 'http://localhost:3010' },
+        { context: '/desktop-signin', target: 'http://localhost:3010' },
         { context: '/api', target: 'http://localhost:3010' },
         { context: '/socket.io', target: 'http://localhost:3010', ws: true },
         { context: '/graphql', target: 'http://localhost:3010' },
