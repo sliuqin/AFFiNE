67f875e32d9a940c42a61d0ac2e7d9cf1baf3bb2
DarkSky <darksky2048@gmail.com>
2025-01-02 23:16:33 +0800

    fix: ava ts loader

--- packages/backend/server/src/__tests__/ava.docker.config.js
+++ packages/backend/server/src/__tests__/ava.docker.config.js
@@ -2,6 +2,12 @@ import packageJson from '../package.json' with { type: 'json' };
 
 export default {
   ...packageJson.ava,
+  nodeArguments: [
+    '--trace-sigint',
+    '--loader',
+    'ts-node/esm/transpile-only.mjs',
+    '--es-module-specifier-resolution=node',
+  ],
   environmentVariables: {
     ...packageJson.ava.environmentVariables,
     TS_NODE_PROJECT: './tests/tsconfig.docker.json',
