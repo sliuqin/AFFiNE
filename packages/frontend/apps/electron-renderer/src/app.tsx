MomoFadaly/yaumy

feat(electron): create recording through tray

packages/frontend/apps/electron-renderer/src/app.tsx
@@ -1,3 +1,4 @@
+import type { DocProps } from '@affine/core/blocksuite/initialization';
 import { AffineContext } from '@affine/core/components/context';
 import { WindowsAppControls } from '@affine/core/components/pure/header/windows-app-controls';
 import { AppContainer } from '@affine/core/desktop/components/app-container';
@@ -38,6 +39,11 @@ import { configureBrowserWorkspaceFlavours } from '@affine/core/modules/workspac
 import createEmotionCache from '@affine/core/utils/create-emotion-cache';
 import { apis, events } from '@affine/electron-api';
 import { StoreManagerClient } from '@affine/nbstore/worker/client';
+import type {
+  AttachmentBlockModel,
+  AttachmentBlockProps,
+} from '@blocksuite/affine/blocks';
+import { Text } from '@blocksuite/affine/store';
 import { CacheProvider } from '@emotion/react';
 import { Framework, FrameworkRoot, getCurrentStore } from '@toeverything/infra';
 import { OpClient } from '@toeverything/infra/op';
@@ -168,32 +174,80 @@ function getCurrentWorkspace() {
 }
 
 events?.applicationMenu.onNewPageAction(type => {
-  const currentWorkspace = getCurrentWorkspace();
-  if (!currentWorkspace) {
-    return;
-  }
-  const { workspace, dispose } = currentWorkspace;
-  const editorSettingService = frameworkProvider.get(EditorSettingService);
-  const docsService = workspace.scope.get(DocsService);
-  const editorSetting = editorSettingService.editorSetting;
-
-  const docProps = {
-    note: editorSetting.get('affine:note'),
-  };
   apis?.ui
     .isActiveTab()
     .then(isActive => {
       if (!isActive) {
         return;
       }
+      const currentWorkspace = getCurrentWorkspace();
+      if (!currentWorkspace) {
+        return;
+      }
+      const { workspace, dispose } = currentWorkspace;
+      const editorSettingService = frameworkProvider.get(EditorSettingService);
+      const docsService = workspace.scope.get(DocsService);
+      const editorSetting = editorSettingService.editorSetting;
+
+      const docProps = {
+        note: editorSetting.get('affine:note'),
+      };
       const page = docsService.createDoc({ docProps, primaryMode: type });
       workspace.scope.get(WorkbenchService).workbench.openDoc(page.id);
+      dispose();
     })
     .catch(err => {
       console.error(err);
     });
+});
 
-  dispose();
+events?.recording.onRecordingStatusChanged(status => {
+  (async () => {
+    if ((await apis?.ui.isActiveTab()) && status?.status === 'stopped') {
+      const currentWorkspace = getCurrentWorkspace();
+      if (!currentWorkspace) {
+        return;
+      }
+      const { workspace, dispose } = currentWorkspace;
+      const editorSettingService = frameworkProvider.get(EditorSettingService);
+      const docsService = workspace.scope.get(DocsService);
+      const editorSetting = editorSettingService.editorSetting;
+
+      const docProps: DocProps = {
+        note: editorSetting.get('affine:note'),
+        page: {
+          title: new Text(
+            'Recording ' +
+              (status.appGroup?.name ?? 'System Audio') +
+              ' ' +
+              new Date(status.startTime).toISOString()
+          ),
+        },
+        onLoad: (doc, { noteId }) => {
+          (async () => {
+            const data = await apis?.recording.saveRecording(status.id);
+            if (!data) {
+              return;
+            }
+            const blob = new Blob([data], { type: 'audio/mp3' });
+            const blobId = await doc.workspace.blobSync.set(blob);
+            const attachmentProps: Partial<AttachmentBlockProps> = {
+              name: 'Recording',
+              size: blob.size,
+              type: 'audio/mp3',
+              sourceId: blobId,
+              embed: true,
+            };
+            doc.addBlock('affine:attachment', attachmentProps, noteId);
+          })().catch(console.error);
+        },
+      };
+      const page = docsService.createDoc({ docProps, primaryMode: 'page' });
+      workspace.scope.get(WorkbenchService).workbench.openDoc(page.id);
+
+      dispose();
+    }
+  })().catch(console.error);
 });
 
 events?.applicationMenu.onOpenJournal(() => {

packages/frontend/core/src/blocksuite/initialization/index.ts
@@ -12,6 +12,14 @@ export interface DocProps {
   surface?: Partial<SurfaceBlockProps>;
   note?: Partial<NoteProps>;
   paragraph?: Partial<ParagraphProps>;
+  onLoad?: (
+    doc: Store,
+    props: {
+      noteId: string;
+      paragraphId: string;
+      surfaceId: string;
+    }
+  ) => void;
 }
 
 export function initDocFromProps(doc: Store, props?: DocProps) {
@@ -20,7 +28,11 @@ export function initDocFromProps(doc: Store, props?: DocProps) {
       'affine:page',
       props?.page || { title: new Text('') }
     );
-    doc.addBlock('affine:surface' as never, props?.surface || {}, pageBlockId);
+    const surfaceId = doc.addBlock(
+      'affine:surface' as never,
+      props?.surface || {},
+      pageBlockId
+    );
     const noteBlockId = doc.addBlock(
       'affine:note',
       {
@@ -29,7 +41,16 @@ export function initDocFromProps(doc: Store, props?: DocProps) {
       },
       pageBlockId
     );
-    doc.addBlock('affine:paragraph', props?.paragraph || {}, noteBlockId);
+    const paragraphBlockId = doc.addBlock(
+      'affine:paragraph',
+      props?.paragraph || {},
+      noteBlockId
+    );
+    props?.onLoad?.(doc, {
+      noteId: noteBlockId,
+      paragraphId: paragraphBlockId,
+      surfaceId,
+    });
     doc.history.clear();
   });
 }

sliuqin/AFFiNE

packages/frontend/apps/electron-renderer/src/app.tsx
@@ -1,3 +1,4 @@
+import type { DocProps } from '@affine/core/blocksuite/initialization';
 import { AffineContext } from '@affine/core/components/context';
 import { WindowsAppControls } from '@affine/core/components/pure/header/windows-app-controls';
 import { AppContainer } from '@affine/core/desktop/components/app-container';
@@ -38,6 +39,8 @@ import { configureBrowserWorkspaceFlavours } from '@affine/core/modules/workspac
 import createEmotionCache from '@affine/core/utils/create-emotion-cache';
 import { apis, events } from '@affine/electron-api';
 import { StoreManagerClient } from '@affine/nbstore/worker/client';
+import type { AttachmentBlockProps } from '@blocksuite/affine/model';
+import { Text } from '@blocksuite/affine/store';
 import { CacheProvider } from '@emotion/react';
 import { Framework, FrameworkRoot, getCurrentStore } from '@toeverything/infra';
 import { OpClient } from '@toeverything/infra/op';
@@ -174,32 +177,80 @@ events?.applicationMenu.openAboutPageInSettingModal(() => {
 });
 
 events?.applicationMenu.onNewPageAction(type => {
-  const currentWorkspace = getCurrentWorkspace();
-  if (!currentWorkspace) {
-    return;
-  }
-  const { workspace, dispose } = currentWorkspace;
-  const editorSettingService = frameworkProvider.get(EditorSettingService);
-  const docsService = workspace.scope.get(DocsService);
-  const editorSetting = editorSettingService.editorSetting;
-
-  const docProps = {
-    note: editorSetting.get('affine:note'),
-  };
   apis?.ui
     .isActiveTab()
     .then(isActive => {
       if (!isActive) {
         return;
       }
+      const currentWorkspace = getCurrentWorkspace();
+      if (!currentWorkspace) {
+        return;
+      }
+      const { workspace, dispose } = currentWorkspace;
+      const editorSettingService = frameworkProvider.get(EditorSettingService);
+      const docsService = workspace.scope.get(DocsService);
+      const editorSetting = editorSettingService.editorSetting;
+
+      const docProps = {
+        note: editorSetting.get('affine:note'),
+      };
       const page = docsService.createDoc({ docProps, primaryMode: type });
       workspace.scope.get(WorkbenchService).workbench.openDoc(page.id);
+      dispose();
     })
     .catch(err => {
       console.error(err);
     });
+});
 
-  dispose();
+events?.recording.onRecordingStatusChanged(status => {
+  (async () => {
+    if ((await apis?.ui.isActiveTab()) && status?.status === 'stopped') {
+      const currentWorkspace = getCurrentWorkspace();
+      if (!currentWorkspace) {
+        return;
+      }
+      const { workspace, dispose } = currentWorkspace;
+      const editorSettingService = frameworkProvider.get(EditorSettingService);
+      const docsService = workspace.scope.get(DocsService);
+      const editorSetting = editorSettingService.editorSetting;
+
+      const docProps: DocProps = {
+        note: editorSetting.get('affine:note'),
+        page: {
+          title: new Text(
+            'Recording ' +
+              (status.appGroup?.name ?? 'System Audio') +
+              ' ' +
+              new Date(status.startTime).toISOString()
+          ),
+        },
+        onStoreLoad: (doc, { noteId }) => {
+          (async () => {
+            const data = await apis?.recording.saveRecording(status.id);
+            if (!data) {
+              return;
+            }
+            const blob = new Blob([data], { type: 'audio/mp3' });
+            const blobId = await doc.workspace.blobSync.set(blob);
+            const attachmentProps: Partial<AttachmentBlockProps> = {
+              name: 'Recording',
+              size: blob.size,
+              type: 'audio/mp3',
+              sourceId: blobId,
+              embed: true,
+            };
+            doc.addBlock('affine:attachment', attachmentProps, noteId);
+          })().catch(console.error);
+        },
+      };
+      const page = docsService.createDoc({ docProps, primaryMode: 'page' });
+      workspace.scope.get(WorkbenchService).workbench.openDoc(page.id);
+
+      dispose();
+    }
+  })().catch(console.error);
 });
 
 events?.applicationMenu.onOpenJournal(() => {

packages/frontend/core/src/blocksuite/initialization/index.ts
@@ -12,6 +12,14 @@ export interface DocProps {
   surface?: Partial<SurfaceBlockProps>;
   note?: Partial<NoteProps>;
   paragraph?: Partial<ParagraphProps>;
+  onStoreLoad?: (
+    doc: Store,
+    props: {
+      noteId: string;
+      paragraphId: string;
+      surfaceId: string;
+    }
+  ) => void;
 }
 
 export function initDocFromProps(doc: Store, props?: DocProps) {
@@ -20,7 +28,11 @@ export function initDocFromProps(doc: Store, props?: DocProps) {
       'affine:page',
       props?.page || { title: new Text('') }
     );
-    doc.addBlock('affine:surface' as never, props?.surface || {}, pageBlockId);
+    const surfaceId = doc.addBlock(
+      'affine:surface' as never,
+      props?.surface || {},
+      pageBlockId
+    );
     const noteBlockId = doc.addBlock(
       'affine:note',
       {
@@ -29,7 +41,16 @@ export function initDocFromProps(doc: Store, props?: DocProps) {
       },
       pageBlockId
     );
-    doc.addBlock('affine:paragraph', props?.paragraph || {}, noteBlockId);
+    const paragraphBlockId = doc.addBlock(
+      'affine:paragraph',
+      props?.paragraph || {},
+      noteBlockId
+    );
+    props?.onStoreLoad?.(doc, {
+      noteId: noteBlockId,
+      paragraphId: paragraphBlockId,
+      surfaceId,
+    });
     doc.history.clear();
   });
 }