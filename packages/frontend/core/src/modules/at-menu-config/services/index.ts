d916b41a3228bb69f0b8c682f07d6d31b369da0b
flrande <flrande.ch@gmail.com>
2025-03-20 16:59:14 +0800

    chore: group order

--- packages/frontend/core/src/modules/at-menu-config/services/index.ts
+++ packages/frontend/core/src/modules/at-menu-config/services/index.ts
@@ -424,10 +424,10 @@ export class AtMenuConfigService extends Service {
   private getMenusFn(): LinkedWidgetConfig['getMenus'] {
     return (query, close, editorHost, inlineEditor, abortSignal) => {
       return [
-        this.memberGroup(query, close, inlineEditor, abortSignal),
         this.journalGroup(query, close, inlineEditor),
         this.linkToDocGroup(query, close, inlineEditor, abortSignal),
         this.newDocMenuGroup(query, close, editorHost, inlineEditor),
+        this.memberGroup(query, close, inlineEditor, abortSignal),
       ];
     };
   }
