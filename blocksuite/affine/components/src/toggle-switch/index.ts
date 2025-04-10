5be029253603f60aa2d90486b2d337ec27d6b198
cuikaipeng <767184612@qq.com>
2025-04-10 21:59:03 +0800

    fix(editor): the switch button style set in the TOC does not effect

--- blocksuite/affine/components/src/toggle-switch/index.ts
+++ blocksuite/affine/components/src/toggle-switch/index.ts
@@ -64,7 +64,7 @@ export class ToggleSwitch extends LitElement {
 
   override render() {
     return html`
-      <label class=${this.on ? 'on' : ''}>
+      <label class=${this.on ? 'subscribe' : ''}>
         <input
           type="checkbox"
           class="switch"
