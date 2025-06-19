Structurate/monorepo

feat(core): support render stream objects in components

packages/frontend/core/src/blocksuite/ai/chat-panel/chat-panel-messages.ts
@@ -21,8 +21,10 @@ import {
   type ChatMessage,
   isChatAction,
   isChatMessage,
+  StreamObjectSchema,
 } from '../components/ai-chat-messages';
 import { type AIError, AIProvider, UnauthorizedError } from '../provider';
+import { mergeStreamObjects } from '../utils/stream-objects';
 import { type ChatContextValue } from './chat-context';
 import { HISTORY_IMAGE_ACTIONS } from './const';
 import { AIPreloadConfig } from './preload-config';
@@ -387,7 +389,12 @@ export class ChatPanelMessages extends WithDisposable(ShadowlessElement) {
         last.content = '';
         last.createdAt = new Date().toISOString();
       }
-      this.updateContext({ messages, status: 'loading', error: null });
+      this.updateContext({
+        messages,
+        status: 'loading',
+        error: null,
+        abortController,
+      });
 
       const { store } = this.host;
       const stream = await AIProvider.actions.chat({
@@ -404,11 +411,23 @@ export class ChatPanelMessages extends WithDisposable(ShadowlessElement) {
         reasoning: this._isReasoningActive,
         webSearch: this._isNetworkActive,
       });
-      this.updateContext({ abortController });
+
       for await (const text of stream) {
         const messages = [...this.chatContextValue.messages];
         const last = messages[messages.length - 1] as ChatMessage;
-        last.content += text;
+        try {
+          const streamObjects = StreamObjectSchema.safeParse(JSON.parse(text));
+          if (streamObjects.success) {
+            last.streamObjects = mergeStreamObjects([
+              ...(last.streamObjects ?? []),
+              streamObjects.data,
+            ]);
+          } else {
+            last.content += text;
+          }
+        } catch {
+          last.content += text;
+        }
         this.updateContext({ messages, status: 'transmitting' });
       }
 

packages/frontend/core/src/blocksuite/ai/components/ai-chat-input/ai-chat-input.ts
@@ -16,9 +16,10 @@ import { ChatAbortIcon } from '../../_common/icons';
 import { type AIError, AIProvider } from '../../provider';
 import { reportResponse } from '../../utils/action-reporter';
 import { readBlobAsURL } from '../../utils/image';
+import { mergeStreamObjects } from '../../utils/stream-objects';
 import type { ChatChip, DocDisplayConfig } from '../ai-chat-chips/type';
 import { isDocChip } from '../ai-chat-chips/utils';
-import type { ChatMessage } from '../ai-chat-messages';
+import { type ChatMessage, StreamObjectSchema } from '../ai-chat-messages';
 import { MAX_IMAGE_COUNT } from './const';
 import type {
   AIChatInputContext,
@@ -610,7 +611,19 @@ export class AIChatInput extends SignalWatcher(
       for await (const text of stream) {
         const messages = [...this.chatContextValue.messages];
         const last = messages[messages.length - 1] as ChatMessage;
-        last.content += text;
+        try {
+          const streamObjects = StreamObjectSchema.safeParse(JSON.parse(text));
+          if (streamObjects.success) {
+            last.streamObjects = mergeStreamObjects([
+              ...(last.streamObjects ?? []),
+              streamObjects.data,
+            ]);
+          } else {
+            last.content += text;
+          }
+        } catch {
+          last.content += text;
+        }
         this.updateContext({ messages, status: 'transmitting' });
       }
 

packages/frontend/core/src/blocksuite/ai/peek-view/chat-block-peek-view.ts
@@ -34,10 +34,14 @@ import type {
   AIReasoningConfig,
 } from '../components/ai-chat-input';
 import type { ChatMessage } from '../components/ai-chat-messages';
-import { ChatMessagesSchema } from '../components/ai-chat-messages';
+import {
+  ChatMessagesSchema,
+  StreamObjectSchema,
+} from '../components/ai-chat-messages';
 import type { TextRendererOptions } from '../components/text-renderer';
 import { AIChatErrorRenderer } from '../messages/error';
 import { type AIError, AIProvider } from '../provider';
+import { mergeStreamObjects } from '../utils/stream-objects';
 import { PeekViewStyles } from './styles';
 import type { ChatContext } from './types';
 import { calcChildBound } from './utils';
@@ -345,7 +349,12 @@ export class AIChatBlockPeekView extends LitElement {
         last.id = '';
         last.createdAt = new Date().toISOString();
       }
-      this.updateContext({ messages, status: 'loading', error: null });
+      this.updateContext({
+        messages,
+        status: 'loading',
+        error: null,
+        abortController,
+      });
 
       const { store } = this.host;
       const stream = await AIProvider.actions.chat({
@@ -362,11 +371,22 @@ export class AIChatBlockPeekView extends LitElement {
         webSearch: this._isNetworkActive,
       });
 
-      this.updateContext({ abortController });
       for await (const text of stream) {
         const messages = [...this.chatContext.messages];
         const last = messages[messages.length - 1] as ChatMessage;
-        last.content += text;
+        try {
+          const streamObjects = StreamObjectSchema.safeParse(JSON.parse(text));
+          if (streamObjects.success) {
+            last.streamObjects = mergeStreamObjects([
+              ...(last.streamObjects ?? []),
+              streamObjects.data,
+            ]);
+          } else {
+            last.content += text;
+          }
+        } catch {
+          last.content += text;
+        }
         this.updateContext({ messages, status: 'transmitting' });
       }
 

sliuqin/AFFiNE

packages/frontend/core/src/blocksuite/ai/chat-panel/chat-panel-messages.ts
@@ -21,8 +21,10 @@ import {
   type ChatMessage,
   isChatAction,
   isChatMessage,
+  StreamObjectSchema,
 } from '../components/ai-chat-messages';
 import { type AIError, AIProvider, UnauthorizedError } from '../provider';
+import { mergeStreamObjects } from '../utils/stream-objects';
 import { type ChatContextValue } from './chat-context';
 import { HISTORY_IMAGE_ACTIONS } from './const';
 import { AIPreloadConfig } from './preload-config';
@@ -387,7 +389,12 @@ export class ChatPanelMessages extends WithDisposable(ShadowlessElement) {
         last.content = '';
         last.createdAt = new Date().toISOString();
       }
-      this.updateContext({ messages, status: 'loading', error: null });
+      this.updateContext({
+        messages,
+        status: 'loading',
+        error: null,
+        abortController,
+      });
 
       const { store } = this.host;
       const stream = await AIProvider.actions.chat({
@@ -404,11 +411,23 @@ export class ChatPanelMessages extends WithDisposable(ShadowlessElement) {
         reasoning: this._isReasoningActive,
         webSearch: this._isNetworkActive,
       });
-      this.updateContext({ abortController });
+
       for await (const text of stream) {
         const messages = [...this.chatContextValue.messages];
         const last = messages[messages.length - 1] as ChatMessage;
-        last.content += text;
+        try {
+          const parsed = StreamObjectSchema.safeParse(JSON.parse(text));
+          if (parsed.success) {
+            last.streamObjects = mergeStreamObjects([
+              ...(last.streamObjects ?? []),
+              parsed.data,
+            ]);
+          } else {
+            last.content += text;
+          }
+        } catch {
+          last.content += text;
+        }
         this.updateContext({ messages, status: 'transmitting' });
       }
 

packages/frontend/core/src/blocksuite/ai/components/ai-chat-input/ai-chat-input.ts
@@ -16,9 +16,10 @@ import { ChatAbortIcon } from '../../_common/icons';
 import { type AIError, AIProvider } from '../../provider';
 import { reportResponse } from '../../utils/action-reporter';
 import { readBlobAsURL } from '../../utils/image';
+import { mergeStreamObjects } from '../../utils/stream-objects';
 import type { ChatChip, DocDisplayConfig } from '../ai-chat-chips/type';
 import { isDocChip } from '../ai-chat-chips/utils';
-import type { ChatMessage } from '../ai-chat-messages';
+import { type ChatMessage, StreamObjectSchema } from '../ai-chat-messages';
 import { MAX_IMAGE_COUNT } from './const';
 import type {
   AIChatInputContext,
@@ -610,7 +611,19 @@ export class AIChatInput extends SignalWatcher(
       for await (const text of stream) {
         const messages = [...this.chatContextValue.messages];
         const last = messages[messages.length - 1] as ChatMessage;
-        last.content += text;
+        try {
+          const parsed = StreamObjectSchema.safeParse(JSON.parse(text));
+          if (parsed.success) {
+            last.streamObjects = mergeStreamObjects([
+              ...(last.streamObjects ?? []),
+              parsed.data,
+            ]);
+          } else {
+            last.content += text;
+          }
+        } catch {
+          last.content += text;
+        }
         this.updateContext({ messages, status: 'transmitting' });
       }
 

packages/frontend/core/src/blocksuite/ai/peek-view/chat-block-peek-view.ts
@@ -34,10 +34,14 @@ import type {
   AIReasoningConfig,
 } from '../components/ai-chat-input';
 import type { ChatMessage } from '../components/ai-chat-messages';
-import { ChatMessagesSchema } from '../components/ai-chat-messages';
+import {
+  ChatMessagesSchema,
+  StreamObjectSchema,
+} from '../components/ai-chat-messages';
 import type { TextRendererOptions } from '../components/text-renderer';
 import { AIChatErrorRenderer } from '../messages/error';
 import { type AIError, AIProvider } from '../provider';
+import { mergeStreamObjects } from '../utils/stream-objects';
 import { PeekViewStyles } from './styles';
 import type { ChatContext } from './types';
 import { calcChildBound } from './utils';
@@ -345,7 +349,12 @@ export class AIChatBlockPeekView extends LitElement {
         last.id = '';
         last.createdAt = new Date().toISOString();
       }
-      this.updateContext({ messages, status: 'loading', error: null });
+      this.updateContext({
+        messages,
+        status: 'loading',
+        error: null,
+        abortController,
+      });
 
       const { store } = this.host;
       const stream = await AIProvider.actions.chat({
@@ -362,11 +371,22 @@ export class AIChatBlockPeekView extends LitElement {
         webSearch: this._isNetworkActive,
       });
 
-      this.updateContext({ abortController });
       for await (const text of stream) {
         const messages = [...this.chatContext.messages];
         const last = messages[messages.length - 1] as ChatMessage;
-        last.content += text;
+        try {
+          const parsed = StreamObjectSchema.safeParse(JSON.parse(text));
+          if (parsed.success) {
+            last.streamObjects = mergeStreamObjects([
+              ...(last.streamObjects ?? []),
+              parsed.data,
+            ]);
+          } else {
+            last.content += text;
+          }
+        } catch {
+          last.content += text;
+        }
         this.updateContext({ messages, status: 'transmitting' });
       }
 